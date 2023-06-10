a096d640071ed065e66d5c0d7f08e6bde87fd028
Lucas Seeger <seegerlucas295@gmail.com>
2023-06-10 15:29:24 -0600

    changed baseDirectory

--- webpack/base.js
+++ webpack/base.js
@@ -34,7 +34,7 @@ module.exports = {
       WEBGL_RENDERER: JSON.stringify(true)
     }),
     new HtmlWebpackPlugin({
-      template: "./src/index.js"
+      template: "./index.html"
     })
   ]
 };
